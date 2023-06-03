8125a827c403584209f755f9645f2bdd48ceb6e8
RegisIocus <133105304+RegisIocus@users.noreply.github.com>
2023-06-03 14:33:13 +0300

    Add files via upload

---
+++ DZ.bat
@@ -0,0 +1,19 @@
+echo off
+md Antoncev
+cd Antoncev
+md Viktor
+md Denisovich
+echo  04051999.txt
+cd Denisovich
+echo  UNPC.txt
+pause
+cd ..
+cd ..
+del Antoncev SQF
+pause
+cd Antoncev
+rd Viktor
+rd Denisovich
+cd..
+rd Antoncev
+pause
