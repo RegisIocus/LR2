6ac8b53600fff057160b131ad8b067f66053d178
RegisIocus <133105304+RegisIocus@users.noreply.github.com>
2023-06-03 14:28:11 +0300

    Add files via upload

---
+++ 6.bat
@@ -0,0 +1,10 @@
+echo off
+rem go to into firstcat
+cd firstcat
+rem create firstfile
+echo > firstfile.txt
+rem go to into secondcat
+cd secondcat
+rem create secondfile
+echo > thirdfile.txt
+pause
